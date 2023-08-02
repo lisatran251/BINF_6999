c3af9d1a22bf19b5371f5a8c52a1b5d9455f73cf
lisatran251 <lisatran251>
2023-08-02 19:15:21 -0400

    scenario 3 update

--- blast.sh
+++ blast.sh
@@ -2,4 +2,4 @@
 
 module load nixpkgs/16.09  gcc/5.4.0 blast+/2.6.0
 
-blastn -query mismatches_primers.txt -subject abricate_only.fasta -word_size 10 -evalue 0.01 -gapopen 5 -gapextend 2 -out abricate_only_blast_result.txt 
+blastn -query mismatches_primers.txt -subject abricate_only.fasta -word_size 10 -evalue 0.01 -gapopen 5 -gapextend 2 -out abricate_only_blast_report.txt 
